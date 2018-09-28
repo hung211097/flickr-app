847fc0afa1667a661564c157f50323892b91d012
hung211097 <1512227@student.hcmus.edu.vn>
2018-09-28 23:13:29 +0700

    update api

--- src/services/api.services.js
+++ src/services/api.services.js
@@ -1,21 +1,14 @@
 import {config} from '../config'
-import {getListIDPhotos, getInfoPhoto} from './utils.services'
+import {getArrayPhotoMedium} from './utils.services'
 import axios from 'axios';
 
 export default () => {
     let services = {
         getInterestPhotos: (page = 1, limit = 10) => {
-          return axios.get(`${config.apiUrl}?method=flickr.interestingness.getList&api_key=${config.KEY_API}&format=json&nojsoncallback=1&page=${page}&per_page=${limit}`).then((data) => {
-            return getListIDPhotos(data.data.photos)
+          return axios.get(`${config.apiUrl}?method=flickr.interestingness.getList&api_key=${config.KEY_API}&extras=media%2Curl_m%2Cowner_name&format=json&nojsoncallback=1&page=${page}&per_page=${limit}`).then((data) => {
+            return getArrayPhotoMedium(data.data.photos)
           })
         },
-        getInfoPhotoById: (id) => {
-          let info = axios.get(`${config.apiUrl}?method=flickr.photos.getInfo&api_key=${config.KEY_API}&format=json&nojsoncallback=1&photo_id=${+id}`)
-          let size = axios.get(`${config.apiUrl}?method=flickr.photos.getSizes&api_key=${config.KEY_API}&format=json&nojsoncallback=1&photo_id=${+id}`)
-          return Promise.all([info, size]).then(([infoPhoto, sizePhoto]) => {
-            return getInfoPhoto(infoPhoto.data.photo, sizePhoto.data.sizes.size)
-          })
-        }
     }
 
     return services
